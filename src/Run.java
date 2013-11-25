5e364ab82f41fe8f38ca6f8cb1be6702820adebe
Andrew Evans <andrewevans94@gmail.com>
2013-11-25 16:51:11 -0500

    stuff!

--- src/Run.java
+++ src/Run.java
@@ -1,10 +1,21 @@
-//testing from andrew! 'w'
 
 public class Run {
 
 	public static void main(String[] args) {
+		int pingRate = 1000;
 		Board b = new Board();
 		System.out.println(b.toString());
+		URLSendReceive butler = new URLSendReceive();
+		Date time = new Date();
+		
+		while (true) {
+			Date currentTime = new Date();
+			if (currentTime.UTC() - time.UTC() >= pingRate) {
+				//do a move
+				time = new Date();
+				System.out.println("We made a move at " + time);
+			}
+		}
 	}
 
 }
