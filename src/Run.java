449b795efa9b6a5300ef2a7d6d4b3646a5d9d658
andrew-evans <andrewevans94@gmail.com>
2013-12-08 22:53:48 -0500

    removed prints

--- src/Run.java
+++ src/Run.java
@@ -142,8 +142,6 @@ public class Run {
 	}
 
 	public static int maxValue(Board state, boolean white, int depth) {
-		System.out.println("Depth: " + depth);
-		System.out.println("is white: " + white);
 		if (depth > 3) {
 			return state.value(white);
 		}
@@ -164,8 +162,6 @@ public class Run {
 	}
 
 	public static int minValue(Board state, boolean white, int depth) {
-		System.out.println("Depth: " + depth);
-		System.out.println("is white: " + white);
 		if (depth > 3) {
 			return state.value(white);
 		}
